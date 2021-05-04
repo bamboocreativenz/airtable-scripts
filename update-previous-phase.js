fb7748faa483e2b532ecb9915934d99e9ac93553
Daniel Lewis <dan.lewis@bamboocreative.nz>
2021-05-05 11:36:49 +1200

    Update update-previous-phase.js

--- update-previous-phase.js
+++ update-previous-phase.js
@@ -16,37 +16,44 @@ const phaseNumber = Number(phase.match(/\d+/)[0])
 // Than update the survey record with the employee and survey recordIds
 
 let employeeRecordId = ''
-let lastSurvey = {}
+let lastSurveys
 
 // If IM is submitted first
 if (config.employeeIMName) {
     employeeRecordId = await findEmployeeRecordId(config.employeeIMName)
-    lastSurvey = await getlastSurvey(employeeRecordId, phaseNumber)
+    lastSurveys = await orderSurveys(employeeRecordId)
 
 // Else if YM is submitted first
 } else if(config.employeeYMName) {
     employeeRecordId = await findEmployeeRecordId(config.employeeYMName)
-    lastSurvey = await getlastSurvey(employeeRecordId, phaseNumber)
+    lastSurveys = await orderSurveys(employeeRecordId)
 }
 
 // If there is at least 1 previous survey
 // Update the survey with the last survey
-if (lastSurvey) {
+if (lastSurveys) {
     await surveyTable.updateRecordAsync(config.recordId, {
-        "Previous Phases": [{
-            'id': lastSurvey.id
-        }]
+        "Previous Phases": lastSurveys
     })
 }
 async function findEmployeeRecordId(name){
     return employeeQuery.records.find((record) => record.getCellValue('name') == name).id
 }
 
+// Reduce through array of previous phases and find the one which has a Phase number less than 1 than the current
 async function getlastSurvey(employeeRecordId, phaseNumber){
     const surveys = await employeeQuery.getRecord(employeeRecordId).getCellValue('Survey  Records')
     return surveys.reduce((accumulator, record) => {
         return Number(record.name.match(/\d+/)[0]) == phaseNumber - 1
         ? record
         : null
+    })
+}
+
+// Order surveys by newest (highest phase number) to oldest (lowest phase number)
+async function orderSurveys(employeeRecordId){
+    const surveys = await employeeQuery.getRecord(employeeRecordId).getCellValue('Survey  Records')
+    return surveys.sort((p1, p2) => {
+        return Number(p1.name.match(/\d+/)[0]) - Number(p2.name.match(/\d+/)[0])
     })
 }
