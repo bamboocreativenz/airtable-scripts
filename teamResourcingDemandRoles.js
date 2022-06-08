c5255ca44aac77366ef8ad0d094b1a45dcff8a95
Daniel Lewis <dan.lewis@bamboocreative.nz>
2022-06-08 19:30:22 +0200

    Update teamResourcingDemandRoles.js

--- teamResourcingDemandRoles.js
+++ teamResourcingDemandRoles.js
@@ -1,17 +1,25 @@
-const {recordId, allAvailable, roleId} = input.config()
+const {recordId, roleId, weekId} = input.config()
 
 /**
  * Workflow:
- * - Get the roles for each person available (allAvailable) for the week
+ * - Get the available people for the week
+ * - Get the roles for each person available for the week
  * - Filter down to just those who have the role needed (roleId)
  * - Write names as string into 'Available for Role'
  */
 
+const weekTable = base.getTable('Weeks')
 const personTable = base.getTable('People')
 const demandTable = base.getTable('Demand & Assignment')
 
-const availablePeople = await personTable.selectRecordsAsync({recordIds: allAvailable, fields: personTable.fields})
+const allPeople = await personTable.selectRecordsAsync({fields: personTable.fields}) 
+const week = await weekTable.selectRecordAsync(weekId[0])
 
-const people = availablePeople.records.map(p => ({...p, role: p.getCellValue('Roles')})).filter(p => p.role[0].id === roleId[0])
+if(week){
+    const weekAvailable = week.getCellValue('Whos Available').map(p => p.id)
+    const availablePeople = allPeople.records.filter(p => weekAvailable.includes(p.id))
+    const people = availablePeople.map(p => ({...p, role: p.getCellValue('Roles').map(r => r.id)}))
 
-demandTable.updateRecordAsync(recordId, {"Available for Role": people.map(p => p.name).join(', ')})
+    const peopleWithRole = people.filter(p => p.role.includes(roleId[0]))
+    demandTable.updateRecordAsync(recordId, {"Recalculate Available": false, "Available for Role": peopleWithRole.map(p => p.name).join(', ')})
+}
